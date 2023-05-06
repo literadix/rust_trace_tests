272c35e3494d2fe090edb3b01697c696350815c6
literadix <bednarz.hannover@gmail.com>
2023-05-06 22:51:44 +0200

    doc

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ async fn main() -> Result<()> {
     let span = span!(Level::INFO, "main");
     let _guard = span.enter();
 
-    // Open a connection to the mini-redis address.
+    // Open a connection to the mini-redis address (start mini-redis-server)
     let mut client = client::connect("127.0.0.1:6379").await?;
 
     // Set the key "hello" with value "world"
