945b27c6a526a8915671c9619090f28a304da954
literadix <literadix@users.noreply.github.com>
2023-05-11 08:56:49 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,7 @@ async fn hello_string<'life>() -> &'life str {
 
 #[tokio::main]
 async fn main() {
-    //https://docs.rs/spinners/latest/spinners/struct.Spinner.html
-    //https://docs.rs/clap/latest/clap/_derive/_tutorial/index.html
+
     let _guard = init_tracing(true, Level::TRACE);
     let op = hello_string();
 
